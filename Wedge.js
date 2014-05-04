f007f59d2b43cbde0bd68d60dab028cd875002ef
matthewsotoudeh <matthewsot@outlook.com>
2014-05-03 19:23:17 -0700

    custom animator
    You can pass an IWedgeAnimator to control the in/out animations. Comes
    with two default animations!

--- Wedge.js
+++ Wedge.js
@@ -1,38 +1,77 @@
 /// <reference path="jquery.d.ts" />
+
+var SimpleFadeAnimation = (function () {
+    function SimpleFadeAnimation() {
+    }
+    SimpleFadeAnimation.prototype.animateIn = function (overlayId, contentId) {
+        $('#' + overlayId).fadeIn('slow');
+        $('#' + contentId).fadeIn('slow');
+    };
+
+    SimpleFadeAnimation.prototype.animateOut = function (overlayId, contentId, completed) {
+        $('#' + overlayId).fadeOut('slow');
+        $('#' + contentId).fadeOut('show');
+    };
+    return SimpleFadeAnimation;
+})();
+
+//Requires jQuery.GSAP for scale animations
+var ScaleInAnimation = (function () {
+    function ScaleInAnimation() {
+    }
+    ScaleInAnimation.prototype.animateIn = function (overlayId, contentId) {
+        $('#' + overlayId).fadeIn('slow');
+        var content = '#' + contentId;
+        $(content).css('transform', 'matrix(0.2, 0, 0, 0.2, 0, 0)');
+        $(content).animate({
+            scaleX: 1,
+            scaleY: 1,
+            "opacity": 1
+        });
+    };
+    ScaleInAnimation.prototype.animateOut = function (overlayId, contentId, completed) {
+        $('#' + overlayId).fadeOut('slow', completed);
+        $('#' + contentId).animate({
+            scaleX: 0.2,
+            scaleY: 0.2,
+            "opacity": 0
+        });
+    };
+    return ScaleInAnimation;
+})();
+
 /*
 * Displays the Wedge Lightbox.
 * link: A link to the youtube video or picture.
 * title: Text displayed below the content
 * type: The type of link provided - youtube or pic
 */
-function initWedge(link, title, type, doAutoPosition, opacity, allowExit) {
-    /*
-    * Shows the overlay
-    */
+function initWedge(link, title, type, animator, doAutoPosition, opacity, allowExit, overlayId, contentId) {
+    if (typeof animator === "undefined") { animator = new SimpleFadeAnimation; }
     if (typeof doAutoPosition === "undefined") { doAutoPosition = true; }
     if (typeof opacity === "undefined") { opacity = 0.9; }
     if (typeof allowExit === "undefined") { allowExit = true; }
-    $("body").append('<div id="lightOverlay" style="overflow:hidden;top:0px;left:0px;position:fixed;z-index:2147483630;opacity:' + opacity.toString() + ';background-color:#000000;display:none;" />');
-    $("#lightOverlay").css("height", window.innerHeight);
-    $("#lightOverlay").css("width", window.innerWidth);
-    $("#lightOverlay").fadeIn("slow");
+    if (typeof overlayId === "undefined") { overlayId = 'wedge-overlay'; }
+    if (typeof contentId === "undefined") { contentId = 'wedge-content'; }
+    /*
+    * Shows the overlay
+    */
+    var overlay = '#' + overlayId;
+    var content = '#' + contentId;
+
+    $('body').append('<div id="' + overlayId + '" style="overflow:hidden;top:0px;left:0px;position:fixed;z-index:2147483630;opacity:' + opacity.toString() + ';background-color:#000000;display:none;" />');
+    $(overlay).css('height', window.innerHeight);
+    $(overlay).css('width', window.innerWidth);
 
     if (allowExit) {
-        $("#lightOverlay").click("slow", function () {
-            var goodMarginLeft = $("#lightContent").css("margin-left");
-            var toGoLeft = (parseInt(goodMarginLeft.replace('px', '')) - 50);
-            $("#lightContent").animate({
-                opacity: 0,
-                marginLeft: toGoLeft
-            }, function () {
-                if (type == "div") {
-                    $("#" + link).appendTo($("body"));
-                    $("#" + link).hide();
+        $(overlay).click(function () {
+            animator.animateOut(overlayId, contentId, function () {
+                if (type == 'div') {
+                    $('#' + link).appendTo($('body'));
+                    $('#' + link).hide();
                 }
-                $("#lightContent").remove();
-                $("#lightOverlay").fadeOut(function () {
-                    $("#lightOverlay").remove();
-                });
+                $(content).stop().remove();
+                $(overlay).stop().remove();
             });
         });
     }
@@ -40,26 +79,23 @@ function initWedge(link, title, type, doAutoPosition, opacity, allowExit) {
     /*
     * Shows the content
     */
-    $("body").append('<div id="lightContent" style="text-align:center;z-index:2147483641;opacity:0;position:fixed;">');
+    $('body').append('<div id="' + contentId + '" style="text-align:center;z-index:2147483641;opacity:0;position:fixed;">');
     switch (type) {
-        case "youtube":
-            $("#lightContent").append('<iframe id="youtubeFrame" width="853" height="480" src="' + link.replace("/watch?v=", "/embed/").replace(/&.*/, "") + '" frameborder="0" allowfullscreen></iframe><h3 style="color:#A0A0A0;">' + title + '</h3>');
+        case 'youtube':
+            $(content).append('<iframe id="youtubeFrame" width="853" height="480" src="' + link.replace('/watch?v=', '/embed/').replace(/&.*/, '') + '" frameborder="0" allowfullscreen></iframe><h3 style="color:#A0A0A0;">' + title + '</h3>');
             break;
-        case "pic":
-            $("#lightContent").append('<img src="' + link + '" style="max-height:' + (window.innerHeight - 100) + 'px;max-width:' + (window.innerWidth - 100) + 'px;"/><h3 style="color:#A0A0A0;">' + title + '</h3>');
+        case 'pic':
+            $(content).append('<img src="' + link + '" style="max-height:' + (window.innerHeight - 100) + 'px;max-width:' + (window.innerWidth - 100) + 'px;"/><h3 style="color:#A0A0A0;">' + title + '</h3>');
             break;
-        case "div":
-            $("#" + link).show();
-            $("#lightContent").append($("#" + link));
+        case 'div':
+            $('#' + link).show();
+            $(content).append($("#" + link));
     }
     if (doAutoPosition) {
-        $("#lightContent").css({ top: '50%', left: '50%', margin: '-' + ($('#lightContent').height() / 2) + 'px 0 0 -' + ($('#lightContent').width() / 2) + 'px' }); //courtesy of http://archive.plugins.jquery.com/project/autocenter
+        $(content).css({ top: '50%', left: '50%', margin: '-' + ($(content).height() / 2) + 'px 0 0 -' + ($(content).width() / 2) + 'px' }); //courtesy of http://archive.plugins.jquery.com/project/autocenter
     }
-    var goodMarginLeft = $("#lightContent").css("margin-left");
-    $("#lightContent").css("margin-left", (parseInt(goodMarginLeft.replace('px', '')) - 50) + "px");
-    $("#lightContent").animate({
-        "opacity": 1,
-        marginLeft: goodMarginLeft
-    }, "slow");
+    var goodMarginLeft = $(content).css('margin-left');
+    $(content).css('margin-left', goodMarginLeft);
+    animator.animateIn(overlayId, contentId);
 }
 //# sourceMappingURL=Wedge.js.map
--- Wedge.ts
+++ Wedge.ts
@@ -1,62 +1,92 @@
 /// <reference path="jquery.d.ts" />
 
+interface IWedgeAnimator {
+    animateIn(overlayId, contentId): void;
+    animateOut(overlayId, contentId, completed): void;
+}
+
+class SimpleFadeAnimation implements IWedgeAnimator {
+    animateIn(overlayId, contentId): void {
+        $('#' + overlayId).fadeIn('slow');
+        $('#' + contentId).fadeIn('slow');
+    }
+
+    animateOut(overlayId, contentId, completed): void {
+        $('#' + overlayId).fadeOut('slow');
+        $('#' + contentId).fadeOut('show');
+    }
+}
+
+//Requires jQuery.GSAP for scale animations
+class ScaleInAnimation implements IWedgeAnimator {
+    animateIn(overlayId, contentId) {
+        $('#' + overlayId).fadeIn('slow');
+        var content = '#' + contentId;
+        $(content).css('transform', 'matrix(0.2, 0, 0, 0.2, 0, 0)');
+        $(content).animate({
+            scaleX: 1,
+            scaleY: 1,
+            "opacity": 1
+        });
+    }
+    animateOut(overlayId, contentId, completed) {
+        $('#' + overlayId).fadeOut('slow', completed);
+        $('#' + contentId).animate({
+            scaleX: 0.2,
+            scaleY: 0.2,
+            "opacity": 0
+        });
+    }
+}
+
 /*
  * Displays the Wedge Lightbox.
  * link: A link to the youtube video or picture.
  * title: Text displayed below the content
  * type: The type of link provided - youtube or pic
  */
-function initWedge(link, title, type, doAutoPosition = true, opacity = 0.9, allowExit = true) {
+function initWedge(link, title, type, animator: IWedgeAnimator = new SimpleFadeAnimation, doAutoPosition = true, opacity = 0.9, allowExit = true, overlayId = 'wedge-overlay', contentId = 'wedge-content') {
     /*
      * Shows the overlay
      */
+    var overlay = '#' + overlayId;
+    var content = '#' + contentId;
 
-    $("body").append('<div id="lightOverlay" style="overflow:hidden;top:0px;left:0px;position:fixed;z-index:2147483630;opacity:' + opacity.toString() + ';background-color:#000000;display:none;" />');
-    $("#lightOverlay").css("height", window.innerHeight);
-    $("#lightOverlay").css("width", window.innerWidth);
-    $("#lightOverlay").fadeIn("slow");
+    $('body').append('<div id="' + overlayId + '" style="overflow:hidden;top:0px;left:0px;position:fixed;z-index:2147483630;opacity:' + opacity.toString() + ';background-color:#000000;display:none;" />');
+    $(overlay).css('height', window.innerHeight);
+    $(overlay).css('width', window.innerWidth);
 
     if (allowExit) {
-        $("#lightOverlay").click("slow", () => {
-            var goodMarginLeft = $("#lightContent").css("margin-left");
-            var toGoLeft = (parseInt(goodMarginLeft.replace('px', '')) - 50);
-            $("#lightContent").animate({
-                opacity: 0,
-                marginLeft: toGoLeft
-            }, function () {
-                if (type == "div") {
-                    $("#" + link).appendTo($("body"));
-                    $("#" + link).hide();
+        $(overlay).click(() => {
+            animator.animateOut(overlayId, contentId, () => {
+                if (type == 'div') {
+                    $('#' + link).appendTo($('body'));
+                    $('#' + link).hide();
                 }
-                $("#lightContent").remove();
-                $("#lightOverlay").fadeOut(function () {
-                    $("#lightOverlay").remove();
-                });
+                $(content).stop().remove();
+                $(overlay).stop().remove();
             });
         });
     }
     /*
      * Shows the content
      */
-    $("body").append('<div id="lightContent" style="text-align:center;z-index:2147483641;opacity:0;position:fixed;">');
+    $('body').append('<div id="' + contentId + '" style="text-align:center;z-index:2147483641;opacity:0;position:fixed;">');
     switch (type) {
-        case "youtube":
-            $("#lightContent").append('<iframe id="youtubeFrame" width="853" height="480" src="' + link.replace("/watch?v=", "/embed/").replace(/&.*/, "") + '" frameborder="0" allowfullscreen></iframe><h3 style="color:#A0A0A0;">' + title + '</h3>');
+        case 'youtube':
+            $(content).append('<iframe id="youtubeFrame" width="853" height="480" src="' + link.replace('/watch?v=', '/embed/').replace(/&.*/, '') + '" frameborder="0" allowfullscreen></iframe><h3 style="color:#A0A0A0;">' + title + '</h3>');
             break;
-        case "pic":
-            $("#lightContent").append('<img src="' + link + '" style="max-height:' + (window.innerHeight - 100) + 'px;max-width:' + (window.innerWidth - 100) + 'px;"/><h3 style="color:#A0A0A0;">' + title + '</h3>')
+        case 'pic':
+            $(content).append('<img src="' + link + '" style="max-height:' + (window.innerHeight - 100) + 'px;max-width:' + (window.innerWidth - 100) + 'px;"/><h3 style="color:#A0A0A0;">' + title + '</h3>');
             break;
-        case "div":
-            $("#" + link).show();
-            $("#lightContent").append($("#" + link));
+        case 'div':
+            $('#' + link).show();
+            $(content).append($("#" + link));
     }
     if (doAutoPosition) {
-        $("#lightContent").css({ top: '50%', left: '50%', margin: '-' + ($('#lightContent').height() / 2) + 'px 0 0 -' + ($('#lightContent').width() / 2) + 'px' }); //courtesy of http://archive.plugins.jquery.com/project/autocenter
+        $(content).css({ top: '50%', left: '50%', margin: '-' + ($(content).height() / 2) + 'px 0 0 -' + ($(content).width() / 2) + 'px' }); //courtesy of http://archive.plugins.jquery.com/project/autocenter
     }
-    var goodMarginLeft = $("#lightContent").css("margin-left");
-    $("#lightContent").css("margin-left", (parseInt(goodMarginLeft.replace('px', '')) - 50) + "px");
-    $("#lightContent").animate({
-        "opacity": 1,
-        marginLeft: goodMarginLeft
-    }, "slow");
+    var goodMarginLeft = $(content).css('margin-left');
+    $(content).css('margin-left', goodMarginLeft);
+    animator.animateIn(overlayId, contentId);
 }
