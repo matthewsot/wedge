8cb8fcd8d789fd1998728806423819c7f0201cc9
matthewsotoudeh <matthewsot@outlook.com>
2015-02-21 15:44:12 -0800

    renamed the animations
    first letter is now lowercased

--- Javascript/Animations.js
+++ Javascript/Animations.js
@@ -1,50 +1,53 @@
-/// <reference path="jquery.d.ts" />
-/// <reference path="wedge.ts" />
 //Animations that require other libraries to work
 //Requires jquery.GSAP: http://www.greensock.com/jquery-gsap-plugin/
-var ScaleAnimation = (function () {
-    function ScaleAnimation() {
-    }
-    ScaleAnimation.prototype.animateIn = function (overlayId, contentId) {
-        $('#' + overlayId).fadeIn('slow');
-        var content = '#' + contentId;
-        $(content).css('transform', 'matrix(0.2, 0, 0, 0.2, 0, 0)');
+var scaleAnimation = (function () {
+    function scaleAnimation() {}
+
+    scaleAnimation.prototype.animateIn = function (overlay, content, completed) {
+        $(overlay).fadeIn("slow", completed);
+
+        $(content).css("transform", "matrix(0.2, 0, 0, 0.2, 0, 0)");
         $(content).animate({
             scaleX: 1,
             scaleY: 1,
             "opacity": 1
         });
     };
-    ScaleAnimation.prototype.animateOut = function (overlayId, contentId, completed) {
-        $('#' + overlayId).fadeOut('slow', completed);
-        $('#' + contentId).animate({
+
+    scaleAnimation.prototype.animateOut = function (overlay, content, completed) {
+        $(overlay).fadeOut("slow", completed);
+        $(content).animate({
             scaleX: 0.2,
             scaleY: 0.2,
             "opacity": 0
         });
     };
-    return ScaleAnimation;
+
+    return scaleAnimation;
 })();
 
 //Requires jQuery.GSAP for scale animations
-var RotateAnimation = (function () {
-    function RotateAnimation() {
-    }
-    RotateAnimation.prototype.animateIn = function (overlayId, contentId) {
-        $('#' + overlayId).fadeIn('slow');
-        var content = '#' + contentId;
-        $(content).css('transform', 'matrix3d(0.93969, 0.34202, 0, 0, -0.34202, 0.93969, 0, 0, 0, 0, 1, -0.05, 0, 0, 0, 1)');
+var rotateAnimation = (function () {
+    function rotateAnimation() {}
+
+    rotateAnimation.prototype.animateIn = function (overlay, content, completed) {
+        $(overlay).fadeIn("slow");
+
+        $(content).css("transform", "matrix3d(0.93969, 0.34202, 0, 0, -0.34202, 0.93969, 0, 0, 0, 0, 1, -0.05, 0, 0, 0, 1)");
         $(content).animate({
             rotationZ: 0,
             "opacity": 1
-        });
+        }, completed);
     };
-    RotateAnimation.prototype.animateOut = function (overlayId, contentId, completed) {
-        $('#' + overlayId).fadeOut('slow', completed);
-        $('#' + contentId).animate({
+
+    rotateAnimation.prototype.animateOut = function (overlay, content, completed) {
+        $(overlay).fadeOut("slow", completed);
+
+        $(content).animate({
             rotationZ: -20,
             "opacity": 0
         });
     };
-    return RotateAnimation;
+
+    return rotateAnimation;
 })();
